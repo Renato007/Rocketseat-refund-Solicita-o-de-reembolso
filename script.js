16efb98c8f6ff2d8bcc47d0e45ee3606ed4c886c
Renato <renatosilsoa@gmail.com>
2025-09-07 18:58:26 -0300

    Criando o ícone da categoria

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const amount = document.getElementById("amount");
 const expense = document.getElementById("expense");
 const category = document.getElementById("category");
 
+//Seleciona os elementos da lista
+const expenseList = document.querySelector("ul")
+
 //capitura o evento de input para formatar o valor
 amount.oninput = () => {
   //Obtém o valor atual do input e remove os caracteres não numéricos
@@ -49,6 +52,16 @@ function expenseAdd(newExpense) {
     const expenseItem = document.createElement("li")
     expenseItem.classList.add("expense")
 
+    //Cria o ícone da categoria
+    const expenseIcon = document.createElement("img")
+    expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`)
+    expenseIcon.setAttribute("alt", newExpense.category_name)
+
+    //Adiciona as informações no item.
+    expenseItem.append(expenseIcon)
+
+    //Adicona o item na lista
+    expenseList.append(expenseItem)
 
   } catch (error) {
     alert("Não foi possível atualizar a lista de despesas.");
