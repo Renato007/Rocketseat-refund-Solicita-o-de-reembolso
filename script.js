40e3640c577ff189750322e924bba04f07d2f4a0
Renato <renatosilsoa@gmail.com>
2025-09-13 20:06:10 -0300

    Criando informações da despesa

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const expense = document.getElementById("expense");
 const category = document.getElementById("category");
 
 //Seleciona os elementos da lista
-const expenseList = document.querySelector("ul")
+const expenseList = document.querySelector("ul");
 
 //capitura o evento de input para formatar o valor
 amount.oninput = () => {
@@ -49,20 +49,34 @@ form.onsubmit = (event) => {
 function expenseAdd(newExpense) {
   try {
     //Cria o elemento para adiconar o item(li) na lista (ul).
-    const expenseItem = document.createElement("li")
-    expenseItem.classList.add("expense")
+    const expenseItem = document.createElement("li");
+    expenseItem.classList.add("expense");
 
     //Cria o ícone da categoria
-    const expenseIcon = document.createElement("img")
-    expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`)
-    expenseIcon.setAttribute("alt", newExpense.category_name)
+    const expenseIcon = document.createElement("img");
+    expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`);
+    expenseIcon.setAttribute("alt", newExpense.category_name);
+
+    //Cria a info da despesa.
+    const expenseInfo = document.createElement("div")
+    expenseInfo.classList.add("expense-info")
+
+    //Criar o nome da despensa.
+    const expenseName = document.createElement("strong")
+    expenseName.textContent = newExpense.expense
+
+    //Cria a categoria da despesa.
+    const expenseCategory = document.createElement("span")
+    expenseCategory.textContent = newExpense.category_name
+
+    //Adiciona name e categoria na div das informações da despesa.
+    expenseInfo.append(expenseName, expenseCategory)
 
     //Adiciona as informações no item.
-    expenseItem.append(expenseIcon)
+    expenseItem.append(expenseIcon, expenseInfo);
 
     //Adicona o item na lista
-    expenseList.append(expenseItem)
-
+    expenseList.append(expenseItem);
   } catch (error) {
     alert("Não foi possível atualizar a lista de despesas.");
     console.log(error);
